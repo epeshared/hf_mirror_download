b39839ae3ad59c24d9ac5eb2d1f7750104ad5922
xtangxtang <xi.tang@intel.com>
2024-08-12 18:01:12 +0800

    update

--- downloadModles.py
+++ downloadModles.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 
 def download_model(model_name, local_dir):
     # 设置环境变量
@@ -9,11 +8,13 @@ def download_model(model_name, local_dir):
     command = f'huggingface-cli download --resume-download {model_name} --local-dir {local_dir}'
     os.system(command)
 
+def download_models_from_file(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            model_name, local_dir = line.strip().split()
+            download_model(model_name, local_dir)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Download models from Hugging Face.')
-    parser.add_argument('model_name', type=str, help='The name of the model to download, e.g., BAAI/bge-large-zh-v1.5')
-    parser.add_argument('local_dir', type=str, help='The local directory where the model should be saved.')
-
-    args = parser.parse_args()
-    download_model(args.model_name, args.local_dir)
-
+    # 假设文件名为 models_to_download.txt
+    file_path = 'models_to_download.txt'
+    download_models_from_file(file_path)
